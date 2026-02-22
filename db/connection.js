8df45ec1f6127ed6596a115e80ed4c787fe19e4d
JamesVeth <javehosting01@gmail.com>
2026-02-22 19:44:11 +0000

    Ready to deploy for Render

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,26 @@
 const { Pool } = require("pg");
-require('dotenv').config();
+const dotenv = require("dotenv");
+
+// ==============================
+// TEMPORARY for test2 DB only
+// process.env.NODE_ENV = "test2";
+// ==============================
+
+let envFile = ".env"; // default
+
+if (process.env.NODE_ENV === "test") {
+  envFile = ".env.test";
+} else if (process.env.NODE_ENV === "test2") {
+  envFile = ".env.test2";
+}
+
+dotenv.config({ path: envFile });
 
 const ENV = process.env.NODE_ENV || "development";
 
 let config = {};
 
-if (ENV === "development" || ENV === "test") {
+if (ENV === "development" || ENV === "test" || ENV === "test2") {
   config = {
     host: process.env.PGHOST,
     user: process.env.PGUSER,
@@ -13,12 +28,10 @@ if (ENV === "development" || ENV === "test") {
     database: process.env.PGDATABASE,
     port: process.env.PGPORT,
   };
-}
-
-if (ENV === "production") {
+} else if (ENV === "production") {
   config = {
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }, // necessary for hosted DB
+    ssl: { rejectUnauthorized: false },
   };
 }
 
